88d10f8c4b0425c4d36b64a5fac5f57807bcab4e
williamoose <williamjliow@gmail.com>
2025-08-20 15:42:25 +0800

    Complete Level 2: Add, List
    Implement an ArrayList from Java's List collection to store tasks.
    Also implement add task function to the list.
    
    My chatbot is now able to print out all the tasks stored in the
    tasklist.

--- src/main/java/Jobe.java
+++ src/main/java/Jobe.java
@@ -1,21 +1,50 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Jobe {
-    private Scanner scanner = new Scanner(System.in);
+  private Scanner scanner;
+  private List<String> taskList;
+  boolean isExit = false;
 
-    public void run() {
-        System.out.println("Hello! I'm Jobe.");
-        System.out.println("What can I do for you?");
-        String input = scanner.nextLine();
-        while (!input.equals("bye")) {
-            System.out.println(input);
-            input = scanner.nextLine();
-        }
+  public Jobe() {
+    scanner = new Scanner(System.in);
+    taskList = new ArrayList<>();
+  }
+
+  public void run() {
+    System.out.println("Hello! I'm Jobe.");
+    System.out.println("What can I do for you?");
+
+  while (!this.isExit) {
+    String input = this.scanner.nextLine();
+    
+    switch (input) {
+      case "bye":
         System.out.println("Bye. Hope to see you again soon!");
+        this.isExit = true;
+        break;
+      case "list":
+        this.printList();
+        break;
+      default:
+        this.taskList.add(input);
+        System.out.println("added: " + input);
+        break;
+      }
     }
+  }
 
-    public static void main(String[] args) {
-        Jobe jobe = new Jobe();
-        jobe.run();
+  public void printList() {
+    int count = 1;
+    for (String task : taskList) {
+      System.out.println(count + ". " + task);
+      count++;
     }
+  }
+
+  public static void main(String[] args) {
+    Jobe jobe = new Jobe();
+    jobe.run();
+  }
 }
