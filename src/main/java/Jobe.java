9927fcaeb66de68547993dc8868225db591c64d3
williamoose <williamjliow@gmail.com>
2025-08-20 15:14:20 +0800

    Complete Level 1: Echo
    Complete Level 1: Echo, to enable my chatbot to echo back the inputs
    user entered into my chatbot.

--- src/main/java/Jobe.java
+++ src/main/java/Jobe.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Jobe {
-    public static void main(String[] args) {
+    private Scanner scanner = new Scanner(System.in);
+
+    public void run() {
         System.out.println("Hello! I'm Jobe.");
         System.out.println("What can I do for you?");
-        System.out.println("_________________________");
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        Jobe jobe = new Jobe();
+        jobe.run();
     }
 }
