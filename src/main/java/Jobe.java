850ee435b1a1970307925fe3268da3b0e1a71271
williamoose <williamjliow@gmail.com>
2025-08-20 14:53:05 +0800

    Complete Level 0: Rename, Greet, Exit

---
+++ src/main/java/Jobe.java
@@ -0,0 +1,8 @@
+public class Jobe {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Jobe.");
+        System.out.println("What can I do for you?");
+        System.out.println("_________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
