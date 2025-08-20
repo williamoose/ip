3fbd3e4a9ef29c7ba62c6072bbe092d66848bd7b
williamoose <williamjliow@gmail.com>
2025-08-20 19:29:33 +0800

    Add automated text UI testing
    Test chatbot using the I/O redirection technique.
    Add EXPECTED.TXT to show expected output when chatbot is fed with a
    fixed number of inputs found in input.txt
    Test outcome is PASS

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,15 +12,20 @@ then
     rm ACTUAL.TXT
 fi
 
+# Find all .java files recursively under src/main/java
+find ../src/main/java -name "*.java" > sources.txt
+
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin @sources.txt
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
+echo "********** BUILD SUCCESS **********"
+
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin Jobe < ../text-ui-test/input.txt > ../text-ui-test/ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
